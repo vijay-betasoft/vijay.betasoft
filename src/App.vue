d28f89b5edf7044ef8ce0a15bfa6185b938a8d8e
vijay-betasoft <vijay.betasoftsolutions@outlook.com>
2023-10-13 14:52:36 +0530

    files of project home-demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HomeDemo  @dataPassed="showdata"/>
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import HomeDemo from './components/HomeDemo.vue'
+// import  EmitDemo from './components/EmitDemo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      HomeDemo
+  },
+  methods:{
+    showdata(val){
+      console.log('emiited value', val);
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#app>div {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #084179;
+  margin-top: 30px;
+  background-color: powderblue;
+}
+#app>div>div>h2{
   margin-top: 60px;
+  background-color: aqua;
 }
 </style>
---
+++ src/components/HomeDemo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>First Page</h1>
+        <button @click="Emitdata" style="width:60px">click me</button>
+    </div>
+</template>
+<script>
+  export default {
+    name: "HomeDemo",
+    data(){
+      return{
+        data:"hello world!"
+      };
+    },
+    methods:{
+      Emitdata(){
+        console.log("inside Emitdata");
+        this.$emit('dataPassed',this.data);
+      },
+    },
+  };
+</script>
